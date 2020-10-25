7267b8f449de8ed17d8997a69cec629afde481f5
eprudenkova <eprudenkova@yahoo.com>
2020-10-25 08:11:20 -0400

    Task8 Add Sum

--- src/com/syntax/tasks/GroupTask8_2DArrayOddNumbers.java
+++ src/com/syntax/tasks/GroupTask8_2DArrayOddNumbers.java
@@ -3,14 +3,18 @@ package com.syntax.tasks;
 public class GroupTask8_2DArrayOddNumbers {
 	public static void main(String[] args) {
 //		Create a 2D array to store numbers and calculate sum of all odd numbers.
-
+		int sum = 0;
 		int[][] array = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { -9, -10, -11, -12 } };
 		for (int[] arr : array) {
 			for (int num : arr) {
 				if (num % 2 != 0) {
 					System.out.print(num + " ");
+					sum+=num;
+
 				}
 			}
 		}
+		System.out.println();
+		System.out.println("The sum of odd numbers is "+sum);
 	}
 }
